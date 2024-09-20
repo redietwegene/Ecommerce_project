76e38726ecc7e9d4006b069d117d49594dd17843
Rediet Haileab <wegenerediet@gmail.com>
2024-09-20 23:58:05 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ function App() {
   return (
     <div>
        <Navbar/>
-      {/* <Product /> */}
-      <Prodcutlist />
+      <Product />
+      {/* <Prodcutlist /> */}
   
     <Admin/>
 <div>
